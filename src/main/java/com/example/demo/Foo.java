5d7487e8397977c768de0482d314e993cefd0a0c
JonasXPX <jonasdefarias@hotmail.com>
2022-06-07 22:38:10 -0300

    fix id

--- src/main/java/com/example/demo/Foo.java
+++ src/main/java/com/example/demo/Foo.java
@@ -1,12 +1,15 @@
 package com.example.demo;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Foo {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     private String name;
